9989c5702fe4f2b6e258239a23dd9659b952e350
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-06 16:36:31 +0200

    add start scipt and process.env.PORT for Vercel

--- app/backend/server.js
+++ app/backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(cors()); 
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 const UserRouter = require('./routes/users')
