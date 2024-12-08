394ded1c616a8f3786f4508578e50b97b71436cf
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-08 13:48:08 +0200

    add cors options

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require('./mongoDB/mongoDB-config');
-const cookieParser = require('cookie-parser');
+const mongoose = require("./mongoDB/mongoDB-config");
+const cookieParser = require("cookie-parser");
+const CORS_OPTIONS = {
+  origin: "http://localhost:5173", // Allow requests from this origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  credentials: true, // Allow cookies
+};
 const app = express();
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(cors()); 
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cors(CORS_OPTIONS));
 app.use(express.json());
 const port = process.env.PORT || 5000;
 app.listen(port);
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Backend is running!" });
-  });
+  res.status(200).json({ message: "Backend is running!" });
+});
 
-const UserRouter = require('./routes/users')
+const UserRouter = require("./routes/users");
 
-app.use("/api/users" , UserRouter)
-
-
-
-
-
+app.use("/api/users", UserRouter);
