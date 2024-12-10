ca17f7f9dbd9683bc0cc4b2cb504ebb4fec689dc
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-10 15:16:31 +0200

    update cors options

--- app/backend/server.js
+++ app/backend/server.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 const mongoose = require("./mongoDB/mongoDB-config");
 const cookieParser = require("cookie-parser");
 const CORS_OPTIONS = {
-  origin: ["http://localhost:5173/", process.env.FRONTEND_URL], // Allow requests from this origin
+  origin: ["http://localhost:5173", process.env.FRONTEND_URL].filter(Boolean), // Allow requests from this origin
   methods: ["GET", "POST", "PUT", "DELETE", 'OPTIONS'], // Allowed HTTP methods
-  credentials: true, // Allow cookies
+  credentials: true, 
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  exposedHeaders: ["set-cookie"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 const app = express();
 app.use(cookieParser(process.env.COOKIE_SECRET));
