7a19199ee90b54bf4e7645f4db5ece93f2d64338
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-12 14:19:39 +0200

    UPDATE wrap routes in context provider and add useState hook to manage the authentication state

--- app/frontend/app/src/App.jsx
+++ app/frontend/app/src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+import HomeView from "./views/HomeView";
+import AuthContext from "./context/AuthContext";
+import  Authentication  from "./hoc/Authentication";
+import AuthView from "./views/AuthView";
 
 function App() {
-  const Home = () => (
-    <div>
-      <h1>Home page</h1>
-      <a href="">sadadsd</a>
-    </div>
-  );
-
+  const [authValue, setAuthValue] = useState({status: 'false', user:{}});
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthContext.Provider value={{isLoggedIn: authValue,  setIsLoggedIn: setAuthValue }}>
+        <Routes>
+          <Route path="/auth" element={<AuthView/>}/>
+          <Route path="/" element={<Authentication><HomeView /> </Authentication>} />
+        </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
