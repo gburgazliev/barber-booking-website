553588e2aa328d4ea6f65afdcefe16aaeb89fc41
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-12 14:18:12 +0200

    update main

--- app/frontend/app/src/main.jsx
+++ app/frontend/app/src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./components/Header.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="  md:flex-col justify-items-center md:items-center w-full">
-      <Header />
-      <App />
-    </div>
+    <App/>
   </StrictMode>
 );
