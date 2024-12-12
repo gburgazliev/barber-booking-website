253c27b225edd6a80383e4befcfb1eeef6e4ce5d
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-12 14:20:36 +0200

    ADD authentication-service file

---
+++ app/frontend/app/src/service/authentication-service.js
@@ -0,0 +1,53 @@
+import { SERVER_URL } from "../constants/serverUrl";
+
+export const login = async (email, password) => {
+  try {
+    const response = await fetch(SERVER_URL("/api/users/login"), {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(
+        "Error loging in! Make sure you are passing valid email and password"
+      );
+    }
+    const body = await response.json();
+    return body.user;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const register = async (firstname, lastname, email, password) => {
+  try {
+    const response = await fetch(SERVER_URL("api/users/register"), {
+      method: "POST",
+      credentials: "include",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+   const errorMessage = await response.json();
+  throw new Error(errorMessage.message)
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+};
