f3fd1d2fe8178ec0c03bb5134d23ebff07658861
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-15 15:45:39 +0200

    wrap the if statement in useEffect so it executes on render, pass location object to the state object in Navigate component

--- app/frontend/app/src/hoc/Authentication.jsx
+++ app/frontend/app/src/hoc/Authentication.jsx
@@ -1,43 +1,52 @@
 import { createContext, useContext } from "react";
 import { SERVER_URL } from "../constants/serverUrl";
 import { useState, useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import { Form, Navigate, useLocation } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 const Authentication = ({ children }) => {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   const [loading, setIsLoading] = useState(false);
+  const location = useLocation();
 
   // automatically login the user if there is a valid cookie
-  if (!isLoggedIn.status) {
-    (async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(SERVER_URL("api/users/login"), {
-          method: "GET",
-          credentials: "include",
-          mode: "cors",
-        });
+  useEffect(() => {
+    if (!isLoggedIn.status) {
+      (async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch(SERVER_URL("api/users/login"), {
+            method: "GET",
+            credentials: "include",
+            mode: "cors",
+          });
 
-        if (response.ok) {
-          const body = await response.json();
+          if (response.ok) {
+            const body = await response.json();
 
-          setIsLoggedIn({ status: true, user: body.user._doc });
+            setIsLoggedIn({ status: true, user: body.user._doc });
+          }
+          setIsLoading(false);
+        } catch (error) {
+          console.log("Auto-login failed:", error.message);
+          setIsLoading(false);
         }
-        setIsLoading(false);
-      } catch (error) {
-        console.log("Auto-login failed:", error.message);
-        setIsLoading(false);
-      }
-    })();
-  }
+      })();
+    }
+  }, []);
 
   if (loading) {
     return <div>Loading....</div>;
   }
 
   if (!isLoggedIn.status) {
-    return <Navigate to="/auth" state={{ auth: "login" }} replace={true} />;
+    return (
+      <Navigate
+        to="/auth"
+        state={{ auth: "login", from: location }}
+        replace={true}
+      />
+    );
   }
 
   return <>{children}</>;
