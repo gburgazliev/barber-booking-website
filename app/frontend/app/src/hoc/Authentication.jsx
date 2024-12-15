56b30714c1705aa60248f8252db37f7ce2dcf362
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-15 14:38:28 +0200

    update hoc to navigate to login page if token has expired

--- app/frontend/app/src/hoc/Authentication.jsx
+++ app/frontend/app/src/hoc/Authentication.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext } from "react";
 import { SERVER_URL } from "../constants/serverUrl";
 import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 const Authentication = ({ children }) => {
@@ -9,36 +10,33 @@ const Authentication = ({ children }) => {
 
   // automatically login the user if there is a valid cookie
   useEffect(() => {
-   
-    
-      (async () => {
-        try {
-          const response = await fetch(SERVER_URL("api/users/login"), {
-            method: "GET",
-            credentials: "include",
-            mode: "cors",
-          });
+    (async () => {
+      try {
+        const response = await fetch(SERVER_URL("api/users/login"), {
+          method: "GET",
+          credentials: "include",
+          mode: "cors",
+        });
 
-          if (response.ok) {
-            const body = await response.json();
-            setIsLoggedIn({ isLoggedIn: true, user: body.user });
-             setIsLoading(false);
-          }
-         
-        } catch (error) {
-          console.log("Auto-login failed:", error.message);
-          setIsLoading(false)
+        if (response.ok) {
+          const body = await response.json();
+          setIsLoggedIn({ status: true, user: body.user });
         }
-      })();
-    
+        setIsLoading(false);
+      } catch (error) {
+        console.log("Auto-login failed:", error.message);
+        setIsLoggedIn({ status: false, user: {} });
+        setIsLoading(false);
+      }
+    })();
   }, []);
-
- useEffect(() => {
- console.log(isLoggedIn)
- }, [isLoggedIn])
 
   if (loading) {
     return <div>Loading....</div>;
+  }
+
+  if (!isLoggedIn.status) {
+    return <Navigate to="/auth" state={{ auth: "login" }} replace={true} />;
   }
 
   return <>{children}</>;
