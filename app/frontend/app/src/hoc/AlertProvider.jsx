3ff90e33bbc7e47652ffbdd398eb8cdbdd2d6133
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-21 00:05:58 +0200

    add AlertProvider component

---
+++ app/frontend/app/src/hoc/AlertProvider.jsx
@@ -0,0 +1,39 @@
+import { useReducer } from "react";
+import alertReducer from "../helpers/alertReducer";
+import AlertContext from "../context/AlertContext";
+import ALERT_TYPES from "../constants/alertTypeConstants";
+import ALERT_ACTIONS from "../constants/alertActionsConstants";
+
+const AlertProvider = ({ children }) => {
+  const [alerts, dispatch] = useReducer(alertReducer, []);
+
+  const removeAlert = (id) => {
+    dispatch({
+      type: ALERT_ACTIONS.REMOVE_ALERT,
+      payload: id,
+    });
+  };
+/**
+ * 
+ * @param {string} message 
+ * @param {string} type 
+ * @param {Number} timeout 5000 ms
+ */
+  const addAlert = (message = "", type = ALERT_TYPES.ERROR, timeout = 5000) => {
+    const id = new Date().getMilliseconds().toString();
+    dispatch({
+      type: ALERT_ACTIONS.ADD_ALERT,
+      payload: { message, type, id },
+    });
+
+    if (timeout) setTimeout(() => removeAlert(id), timeout);
+  };
+
+  return (
+    <AlertContext.Provider value={{ alerts, addAlert, removeAlert }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertProvider;
