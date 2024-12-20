358aa163e9807976ea34e5e2dc4bec2a1f3d4717
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-21 00:15:03 +0200

    wrap all routes in AlertProvider

--- app/frontend/app/src/App.jsx
+++ app/frontend/app/src/App.jsx
@@ -3,21 +3,28 @@ import "./App.css";
 import { useState } from "react";
 import HomeView from "./views/HomeView";
 import AuthContext from "./context/AuthContext";
-import  Authentication  from "./hoc/Authentication";
+import Authentication from "./hoc/Authentication";
 import AuthView from "./views/AuthView";
+import AlertProvider from "./hoc/AlertProvider";
+import AlertContainer from "./components/AlertContainer";
 
 function App() {
-  const [authValue, setAuthValue] = useState({status: false, user:{}});
+  const [authValue, setAuthValue] = useState({ status: false, user: {} });
 
   return (
-    <BrowserRouter>
-      <AuthContext.Provider value={{isLoggedIn: authValue,  setIsLoggedIn: setAuthValue }}>
-        <Routes>
-          <Route path="/auth" element={<AuthView/>}/>
-          <Route path="/" element={<HomeView /> } />
-        </Routes>
-      </AuthContext.Provider>
-    </BrowserRouter>
+    <AlertProvider>
+      <AlertContainer />
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{ isLoggedIn: authValue, setIsLoggedIn: setAuthValue }}
+        >
+          <Routes>
+            <Route path="/auth" element={<AuthView />} />
+            <Route path="/" element={<HomeView />} />
+          </Routes>
+        </AuthContext.Provider>
+      </BrowserRouter>
+    </AlertProvider>
   );
 }
 
