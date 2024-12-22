23ca75c4cb1410fd0dbc7002fa184fa50cd7371f
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-22 17:32:28 +0200

    update user schema

--- app/backend/models/User.js
+++ app/backend/models/User.js
@@ -5,7 +5,9 @@ const UserSchema = new mongoose.Schema({
     lastname: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: String, required: true}
+    role: {type: String, required: true},
+    resetToken: String,
+    resetTokenExpirationTime: Date
 })
 
 const User = mongoose.model('User', UserSchema);
