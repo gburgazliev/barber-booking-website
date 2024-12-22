839f03c4df1763c4dda7b05cd98ae5995c5ce277
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-22 17:36:17 +0200

    update

--- app/frontend/app/src/App.jsx
+++ app/frontend/app/src/App.jsx
@@ -7,6 +7,9 @@ import Authentication from "./hoc/Authentication";
 import AuthView from "./views/AuthView";
 import AlertProvider from "./hoc/AlertProvider";
 import AlertContainer from "./components/AlertContainer";
+import ResetPasswordView from './views/ResetPasswordView';
+import NewPasswordView from "./views/NewPasswordView";
+
 
 function App() {
   const [authValue, setAuthValue] = useState({ status: false, user: {} });
@@ -21,6 +24,8 @@ function App() {
           <Routes>
             <Route path="/auth" element={<AuthView />} />
             <Route path="/" element={<HomeView />} />
+            <Route path="/reset-password" element={<ResetPasswordView />} />
+            <Route path="/reset-password/:resetToken" element={<NewPasswordView />} />
           </Routes>
         </AuthContext.Provider>
       </BrowserRouter>
