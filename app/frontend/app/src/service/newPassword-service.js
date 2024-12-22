0f839a8cb0c73daff4472825398276ceab78230c
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-22 17:38:25 +0200

    add newPassword service file

---
+++ app/frontend/app/src/service/newPassword-service.js
@@ -0,0 +1,23 @@
+import { SERVER_URL } from "../constants/serverUrl";
+
+export const newPasswordRequest = async (resetToken, newPassword) => {
+  try {
+    const response = await fetch(SERVER_URL("api/users/new-password"), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ resetToken, newPassword }),
+    });
+    const body = await response.json();
+
+    if (!response.ok) {
+      throw new Error(body.message);
+    }
+    console.log(response.status)
+
+    return body;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
