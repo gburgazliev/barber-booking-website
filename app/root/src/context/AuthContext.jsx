f8219e1737267684d33221e6bbd1ccb6e0e7af28
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-28 16:53:34 +0200

    update authContext to include loading state

--- app/root/src/context/AuthContext.jsx
+++ app/root/src/context/AuthContext.jsx
@@ -4,6 +4,8 @@ import { createContext, useContext } from "react";
  const AuthContext = createContext({
     isLoggedIn: {status: false, user: {}},
     setIsLoggedIn: () => {},
+    isLoading: true,
+    setIsLoading: () => {},
   });
 
   export default AuthContext;
