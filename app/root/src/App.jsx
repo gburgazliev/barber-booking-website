2f8a404d4704f4b5a381a2cf5a4b36fcb394d353
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-28 17:58:32 +0200

    update

--- app/root/src/App.jsx
+++ app/root/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import HomeView from "./views/HomeView";
 import AuthContext from "./context/AuthContext";
 import Authentication from "./hoc/Authentication";
@@ -10,13 +10,12 @@ import AlertContainer from "./components/AlertContainer";
 import ResetPasswordView from "./views/ResetPasswordView";
 import NewPasswordView from "./views/NewPasswordView";
 import { autoLogin } from "./service/authentication-service";
-import ALERT_TYPES from "./constants/alertTypeConstants";
-import AlertContext from "./context/AlertContext";
+
 
 function App() {
   const [authValue, setAuthValue] = useState({ status: false, user: {} });
   const [isLoading, setIsLoading] = useState(true);
-  const { addAlert } = useContext(AlertContext);
+ 
 
   useEffect(() => {
     const handleAutoLogin = async () => {
