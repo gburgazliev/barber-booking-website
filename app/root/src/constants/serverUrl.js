2afb1727c4210abcceba81ac103496637c2e9bfd
gburgazliev <burgazlievgeorgi@gmail.com>
2024-12-28 15:14:45 +0200

    update

--- app/root/src/constants/serverUrl.js
+++ app/root/src/constants/serverUrl.js
@@ -1,6 +1,1 @@
-export const SERVER_URL = (ENDPOINT) =>{
-    const baseUrl = window.location.hostname === 'localhost' 
-    ? 'http://localhost:5000'
-    : `http://${window.location.hostname}:5000`;  //import.meta.env.VITE_BACKEND_URL_TEST
-    
-    return `${baseUrl}/${ENDPOINT}`}; 
+export const SERVER_URL = (ENDPOINT) => `/${ENDPOINT}`
