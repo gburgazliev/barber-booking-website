c61ff6d19d08f8a367ceebff8c7f5b5bbd1eca95
gburgazliev <burgazlievgeorgi@gmail.com>
2025-01-18 20:54:06 +0200

    refactor AlertContainer to use sweetalert

--- app/root/src/components/AlertContainer.jsx
+++ app/root/src/components/AlertContainer.jsx
@@ -1,101 +1,66 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import AlertContext from "../context/AlertContext";
+import Swal from "sweetalert2";
 import ALERT_TYPES from "../constants/alertTypeConstants";
-
 const AlertContainer = () => {
   const { alerts, removeAlert } = useContext(AlertContext);
-  const alertVariants = {
-    hidden: {
-      opacity: 0,
-      x: 0,
-      y: 0,
-    },
-    visible: {
-      opacity: 1,
-      x: 0,
-      y: 50,
-      transition: { duration: 0.5 },
-    },
-    exit: {
-      opacity: 0,
-      x: 0,
-      y: -100,
-      transition: { duration: 1 },
-    },
-  };
+  const theme = localStorage.getItem("theme");
 
-  return (
-    <motion.div
-      layout
-      className="flex flex-col w-90 text-sm md:text-lg gap-5 right-2  fixed top-10 md:right-10 z-[999]"
-    >
-      <AnimatePresence>
-        {alerts.map((alert) => {
-          switch (alert.type) {
-            case ALERT_TYPES.ERROR: {
-              return (
-                <motion.div
-                  layout
-                  variants={alertVariants}
-                  role="alert"
-                  key={alert.id}
-                  initial="hidden"
-                  animate="visible"
-                  exit="exit"
-                  className="alert alert-error select-none hover:cursor-pointer"
-                  onClick={() => removeAlert(alert.id)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
+  useEffect(() => {
+    if (theme === "forest") {
+      import("@sweetalert2/theme-borderless/borderless.css");
+    } else {
+      import("@sweetalert2/theme-dark/dark.css");
+    }
+  }, [theme]);
 
-                  <span>{alert.text}</span>
-                </motion.div>
-              );
-            }
-            case ALERT_TYPES.SUCCESS: {
-              return (
-                <motion.div
-                layout
-                  variants={alertVariants}
-                  role="alert"
-                  key={alert.id}
-                  className="alert alert-success select-none hover:cursor-pointer"
-                  onClick={() => removeAlert(alert.id)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span>{alert.text}</span>
-                </motion.div>
-              );
-            }
-          }
-        })}
-      </AnimatePresence>
-    </motion.div>
-  );
+  useEffect(() => {
+    if (alerts.length > 0) {
+      const latestAlert = alerts[alerts.length - 1];
+      switch (latestAlert.type) {
+        case ALERT_TYPES.ERROR:
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: latestAlert.text,
+            timer: 3000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            toast: true,
+            position: "top",
+          });
+          break;
+        case ALERT_TYPES.SUCCESS:
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: latestAlert.text,
+            timer: 3000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            toast: true,
+            position: "top",
+          });
+          break;
+
+        default:
+          Swal.fire({
+            icon: "info",
+            text: latestAlert.text,
+            
+           
+            position: "center",
+           
+            backdrop: "rgb(0, 0, 0, 0.3)",
+          });
+      }
+
+      removeAlert(latestAlert.id);
+    }
+  }, [alerts, removeAlert]);
+
+  return null;
 };
 
 export default AlertContainer;
