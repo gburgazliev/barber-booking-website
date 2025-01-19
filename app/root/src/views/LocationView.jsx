2408642fd7914bf9e53a93fd971a2ea7481957a5
gburgazliev <burgazlievgeorgi@gmail.com>
2025-01-19 18:10:01 +0200

    add LocationView

---
+++ app/root/src/views/LocationView.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Location from "../components/Location";
+
+const LocationView = () => {
+ return <div className="flex flex-col w-screen gap-2 md:gap-0">
+    <div className="sticky top-2 z-10">
+        <Header />
+    </div>
+    <div className="flex flex-grow items-center justify-center">
+         <Location />
+    </div>
+   
+  </div>;
+};
+
+export default LocationView;
