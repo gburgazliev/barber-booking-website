d753dffec3e7d6d5c80c9c3cd7d428dbf038b9ec
gburgazliev <burgazlievgeorgi@gmail.com>
2025-02-12 19:21:26 +0200

    add AppointmentSchema

---
+++ app/root/api/models/Appointment.js
@@ -0,0 +1,20 @@
+const { mongoose } = require("../mongoDB/mongoDB-config");
+
+const AppointmentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  type: { type: String, enum:['Hair', 'Hair and Beard', 'Beard'], required: true },
+  date: {type: String, required: true},
+  timeSlot: {type: String, required: true},
+  status: {type: String, enum: ['Pending', 'Confirmed'], default: 'Pending'
+  },
+  confirmationHex: {type: String, required: true},
+  expiresAt: { 
+    type: Date, 
+    default: () => new Date(Date.now() + 600000), 
+    expires: 0 // TTL index enabled
+}
+});
+
+const Appointment = mongoose.model("Appointments", AppointmentSchema);
+
+module.exports = Appointment;
