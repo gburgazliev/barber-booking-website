c9649fd322cc776eb033717fc3fa189b16fc191c
gburgazliev <burgazlievgeorgi@gmail.com>
2025-03-06 15:57:19 +0200

    update Calendar component to use dayjs as date object and pass setCurrentUserAppointments function as prop to Appointment component

--- app/root/src/components/Calendar.jsx
+++ app/root/src/components/Calendar.jsx
@@ -4,18 +4,22 @@ import AlertContext from "../context/AlertContext";
 import Appointment from "./Appointment";
 import AlERT_TYPES from "../constants/alertTypeConstants";
 import { SERVER_URL } from "../constants/serverUrl";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import dayjs from "dayjs";
 
 const Calendar = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const currentDate = new Date();
+  const [date, setDate] = useState(dayjs());
+  const [currentUserAppointments, setCurrentUserAppointments] = useState({});
   const [timeSlots, setTimeSlots] = useState([]);
   const [appointments, setAppointments] = useState([]);
   const { addAlert } = useContext(AlertContext);
   const { isLoggedIn } = useContext(AuthContext);
+  const minDate = dayjs();
 
-  const maxDate = new Date();
-  maxDate.setDate(currentDate.getDate() + 14);
-  const formattedDateString = selectedDate.toISOString().split("T")[0]; // YYYY-MM-DD
+  // Set maximum date to 14 days from today
+  const maxDate = dayjs().add(14, "day");
+
+  const formattedDateString = date.toISOString().split("T")[0]; // YYYY-MM-DD
 
   const getDayOfWeek = (date) => {
     return new Date(date).toLocaleDateString("en-US", { weekday: "long" });
@@ -109,6 +113,8 @@ const Calendar = () => {
     fetchSchedule();
   }, [formattedDateString]);
 
+ 
+
   useEffect(() => {
     const fetchAppointments = async () => {
       try {
@@ -132,101 +138,66 @@ const Calendar = () => {
       setTimeSlots((prev) =>
         prev.filter((timeSlot) => {
           const isBookedByOthers = appointments.some(
-            (appointment) =>
-              timeSlot === appointment.timeSlot &&
-              appointment.userId._id !== isLoggedIn?.user._id
+            (appointment) => timeSlot === appointment.timeSlot
           );
 
           // If it's booked by someone else, filter it out
           if (isBookedByOthers) {
             return false;
+          } else {
+            return true;
           }
 
           // Otherwise keep the time slot (whether it's available or booked by current user)
-          return true;
         })
       );
     }
   }, [appointments, isLoggedIn]);
 
+  // Prevent month changes
+  const handleMonthChange = () => {
+    // Return current month to prevent navigation
+    return dayjs().startOf("month");
+  };
+
   return (
-    <div className="border">
+    <div className="border bg-white">
+      <DateCalendar
+        value={date}
+        onChange={(newDate) => setDate(newDate)}
+        minDate={minDate}
+        maxDate={maxDate}
+        // onMonthChange={handleMonthChange}
+        views={["day"]} // Only show day view, hide year and month pickers
+      />
+      Available appointments
+      <div className="grid grid-cols-4 gap-5">
+        {timeSlots.map((timeSlot, index) => (
+          <Appointment
+            key={index}
+            timeSlot={timeSlot}
+            date={formattedDateString}
+            appointments={appointments}
+            setCurrentUserAppointments={setCurrentUserAppointments}
+          />
+        ))}
+      </div>
       <div className="flex flex-col">
-        <div className="flex justify-between relative">
-          <button
-            disabled={selectedDate <= currentDate}
-            onClick={() =>
-              setSelectedDate(
-                new Date(selectedDate.setDate(selectedDate.getDate() - 1))
-              )
-            }
-          >
-            Previous day
-          </button>
-          <div className="flex gap-1">
-            <h2 className="text-xl font-bold">
-              {selectedDate.toLocaleDateString()} - {getDayOfWeek(selectedDate)}
-            </h2>
-
-            <button
-              className=""
-              onClick={() => document.getElementById("my_modal_1").showModal()}
-            >
-              icon
-            </button>
-            <dialog id="my_modal_1" className="modal">
-              <div className="modal-box">
-                <div className="flex flex-col items-center gap-2 p-2">
-                  <div className="flex flex-col gap-2 p-2 ">
-                    Schedule
-                    <div className="flex gap-2 p-2">
-                      <input type="time" id="start" className="" />
-                      -
-                      <input type="time" id="end" className="" />
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-2 p-2 ">
-                    Break
-                    <div className="flex gap-2 p-2">
-                      <input type="time" id="breakStart" className="" />
-                      -
-                      <input type="time" id="breakEnd" className="" />
-                    </div>
-                  </div>
-
-                  <button className="w-1/4" onClick={handleSaveSchedule}>
-                    Save
-                  </button>
-                </div>
-                <div className="modal-action">
-                  <form method="dialog">
-                    {/* if there is a button in form, it will close the modal */}
-                    <button className="btn">Close</button>
-                  </form>
-                </div>
-              </div>
-            </dialog>
-          </div>
-          <button
-            disabled={selectedDate.getDate() == maxDate.getDate()}
-            onClick={() =>
-              setSelectedDate(
-                new Date(selectedDate.setDate(selectedDate.getDate() + 1))
-              )
-            }
-          >
-            Next day
-          </button>
-        </div>
-        <div className="grid grid-cols-4 gap-5">
-          {timeSlots.map((timeSlot, index) => (
-            <Appointment
-              key={index}
-              timeSlot={timeSlot}
-              date={formattedDateString}
-              appointments={appointments}
-            />
-          ))}
+        Your appointments
+        <div>
+          {appointments
+            .filter(
+              (appointment) => appointment._id === currentUserAppointments._id
+            )
+            .map((appointmentObj) => (
+              <Appointment
+                key={appointmentObj._id}
+                timeSlot={appointmentObj.timeSlot}
+                date={formattedDateString}
+                appointments={appointments}
+                setCurrentUserAppointments={setCurrentUserAppointments}
+              />
+            ))}
         </div>
       </div>
     </div>
