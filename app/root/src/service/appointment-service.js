ddd5e9582ab62f0c87d40289235d7f03fe31baf6
gburgazliev <burgazlievgeorgi@gmail.com>
2025-03-06 15:57:41 +0200

    add cancelAppointment service function

---
+++ app/root/src/service/appointment-service.js
@@ -0,0 +1,8 @@
+ import { SERVER_URL } from "../constants/serverUrl"
+ 
+ export const cancelAppointment = async (id) => {
+    const  response = await fetch(SERVER_URL(`api/appointments/cancel/:${id}`), {
+        method: 'DELETE'
+    });
+    return response;
+}
