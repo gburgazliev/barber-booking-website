fa3a0441156d7eda42b6153bd9005e4377b7547b
gburgazliev <burgazlievgeorgi@gmail.com>
2025-04-17 14:50:19 +0300

    add HomePhotosContainer

--- app/root/src/components/Home.jsx
+++ app/root/src/components/Home.jsx
@@ -1,19 +1,21 @@
-import { useContext , useEffect} from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import { autoLogin } from "../service/authentication-service";
 import AlertContext from "../context/AlertContext";
 import Calendar from "./Calendar";
+import HomePhotosContainer from "./HomePhotosContainer";
 const Home = () => {
   const { isLoggedIn } = useContext(AuthContext);
-  const {addAlert} = useContext(AlertContext);
+  const { addAlert } = useContext(AlertContext);
 
- 
   return (
-    <div className='w-full  flex flex-col justify-center items-center'>
-      
-      <Calendar/>
-      <a href="">sadadsd</a>
-    </div>
+    <div className="w-full pt-10  flex   items-center justify-evenly sm:flex-col md:flex-col lg:flex-row ">
+     
+     
+      <Calendar />
+      <HomePhotosContainer />
+   
+     </div>
   );
 };
 export default Home;
---
+++ app/root/src/components/HomePhotosContainer.jsx
@@ -0,0 +1,102 @@
+import { motion } from "framer-motion";
+import salon1 from "../assets/salon1.png";
+import salon2 from "../assets/salon2.png";
+import salon3 from "../assets/salon3.png";
+import salon4 from "../assets/salon4.png";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { useState } from "react";
+
+const HomePhotosContainer = () => {
+  const photos = [salon1, salon2, salon3, salon4];
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const photoVariants = {
+    visible: {
+      x: 0,
+      y: 0,
+      scale: 0.8,
+      rotate: 0,
+      opacity: 1,
+    },
+    hidden: (i) => ({
+      x: (i % 2 === 0 ? -1 : 1) * 50,
+      y: Math.floor(i / 2) * 50,
+      scale: 1,
+      rotate: -10,
+      opacity: 0,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+      },
+    }),
+  };
+
+  const iconVariants = {
+    initial: { x: 0 },
+    hover: {
+      x: 5,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+        repeat: Infinity,
+        repeatType: "reverse",
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full md:w-1/2 p-4">
+      <motion.div
+        className="grid grid-cols-2 gap-0 select-none"
+        variants={containerVariants}
+        initial="hidden"
+        animate={"visible"}
+      >
+        {photos.map((photo, index) => (
+          <motion.img
+            key={index}
+            custom={index}
+            whileHover={{ rotate: 10 }}
+            whileTap={{ scale: 1.5, zIndex: 10 }}
+            src={photo}
+            alt={`Salon ${index + 1}`}
+            className="bg-green-100 sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[300px] object-cover rounded-lg shadow-lg shadow-green-300 hover:shadow-2xl"
+            variants={photoVariants}
+          />
+        ))}
+      </motion.div>
+
+      <motion.button
+        onHoverStart={() => setIsButtonHovered(true)}
+        onHoverEnd={() => setIsButtonHovered(false)}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+        className="flex items-center gap-2 mt-4 px-4 py-2 rounded-lg bg-gray-400 text-white font-semibold "
+      >
+        Checkout more photos
+        <motion.span
+          variants={iconVariants}
+          initial="initial"
+          className="text-xl"
+          animate={isButtonHovered ? "hover" : "initial"}
+        >
+          <FaRegArrowAltCircleRight />
+        </motion.span>
+      </motion.button>
+    </div>
+  );
+};
+
+export default HomePhotosContainer;
