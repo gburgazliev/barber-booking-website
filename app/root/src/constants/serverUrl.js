5ee9e9753146597d67ed81af069938f56a914bb8
gburgazliev <burgazlievgeorgi@gmail.com>
2025-04-23 00:08:51 +0300

    -

--- app/root/src/constants/serverUrl.js
+++ app/root/src/constants/serverUrl.js
@@ -1,1 +1,1 @@
-export const SERVER_URL = (ENDPOINT) => `${import.meta.env.VITE_BACKEND_URL_TEST}/${ENDPOINT}`
+export const SERVER_URL = (ENDPOINT) => `${import.meta.env.VITE_BACKEND_URL}/${ENDPOINT}`
