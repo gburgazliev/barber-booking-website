b12ab904f63aff1196b9b8e21329c3110cf595fc
gburgazliev <burgazlievgeorgi@gmail.com>
2025-04-26 19:33:17 +0300

    add new fields and pre save middleware

--- app/root/api/models/User.js
+++ app/root/api/models/User.js
@@ -7,8 +7,24 @@ const UserSchema = new mongoose.Schema({
     password: {type: String, required: true, select: false},
     role: {type: String, required: true},
     resetToken: String,
-    resetTokenExpirationTime: Date
+    resetTokenExpirationTime: Date,
+    attendance: {type: Number, default: 0, min: -3, max: 5}, 
+    rights: {type:String, default:'regular', enum:['regular', 'suspended']},
 })
+
+// Pre-save middleware to update rights based on attendance
+UserSchema.pre('save', function(next) {
+    // If attendance is changed OR this is a new document
+    if (this.isModified('attendance') || this.isNew) {
+        // Set rights based on attendance value
+        if (this.attendance <= -3) {
+            this.rights = 'suspended';
+        } else {
+            this.rights = 'regular';
+        }
+    }
+    next();
+});
 
 const User = mongoose.model('User', UserSchema);
 
