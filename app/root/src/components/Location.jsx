6eccba03ddd4dfb5bedf7841bc15b3e9304b883a
gburgazliev <burgazlievgeorgi@gmail.com>
2025-06-01 00:31:37 +0300

    change schedule

--- app/root/src/components/Location.jsx
+++ app/root/src/components/Location.jsx
@@ -193,12 +193,12 @@ const Location = () => {
                 
                 <div className="space-y-3">
                   {[
-                    { day: "Monday", hours: "9:00 AM - 7:00 PM" },
+                    { day: "Monday", hours: "12:00 AM - 7:00 PM" },
                     { day: "Tuesday", hours: "9:00 AM - 7:00 PM" },
                     { day: "Wednesday", hours: "9:00 AM - 7:00 PM" },
                     { day: "Thursday", hours: "9:00 AM - 7:00 PM" },
                     { day: "Friday", hours: "9:00 AM - 7:00 PM" },
-                    { day: "Saturday", hours: "10:00 AM - 6:00 PM" },
+                    { day: "Saturday", hours: "9:00 AM - 6:00 PM" },
                     { day: "Sunday", hours: "Closed" }
                   ].map((schedule, index) => {
                     const today = new Date().getDay();
