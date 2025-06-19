2ad0636bbd0f3de1881b25e646b2c5d47b82a61a
gburgazliev <burgazlievgeorgi@gmail.com>
2025-06-19 18:39:47 +0300

    update

--- app/root/src/context/AuthContext.jsx
+++ app/root/src/context/AuthContext.jsx
@@ -2,10 +2,12 @@ import { createContext, useContext } from "react";
 
 
  const AuthContext = createContext({
-    isLoggedIn: {status: false, user: {}},
-    setIsLoggedIn: () => {},
-    isLoading: true,
-    setIsLoading: () => {},
-  });
+  isLoggedIn: { status: false, user: {} },
+  setIsLoggedIn: () => {},
+  isLoading: true,
+  setIsLoading: () => {},
+  refreshUserData: () => {},
+  clearAuth: () => {},
+});
 
   export default AuthContext;
