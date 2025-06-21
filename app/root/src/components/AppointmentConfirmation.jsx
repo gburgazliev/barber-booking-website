c63187df1c511f56d5c6911d2f2ee91d85621f93
gburgazliev <burgazlievgeorgi@gmail.com>
2025-06-21 09:46:47 +0300

    comment code

--- app/root/src/components/AppointmentConfirmation.jsx
+++ app/root/src/components/AppointmentConfirmation.jsx
@@ -3,10 +3,29 @@ import { useParams } from "react-router-dom";
 import { SERVER_URL } from "../constants/serverUrl";
 import ALERT_TYPES from "../constants/alertTypeConstants";
 import AlertContext from "../context/AlertContext";
+import AuthContext from "../context/AuthContext";
 const AppointmentConfirmation = () => {
   const { addAlert } = useContext(AlertContext);
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   let { confirmationToken } = useParams();
   confirmationToken = confirmationToken.slice(1);
+  // const fetchUpdatedUserData = async () => {
+  //   try {
+  //     const response = await fetch(SERVER_URL("api/users/login"), {
+  //       method: "GET",
+  //       credentials: "include",
+  //       mode: "cors",
+  //     });
+
+  //     if (response.ok) {
+  //       const body = await response.json();
+  //       setIsLoggedIn({ status: true, user: body.user });
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching updated user data:", error);
+  //   }
+  // };
+
   useEffect(() => {
     const confirmAppointment = async () => {
       const response = await fetch(
@@ -21,7 +40,9 @@ const AppointmentConfirmation = () => {
        return;
       }
       addAlert(data.message, ALERT_TYPES.SUCCESS, 'center', false, false, false, null)
-      
+      // if (isLoggedIn.status) {
+      //   await fetchUpdatedUserData();
+      // }
     };
 
     confirmAppointment();
